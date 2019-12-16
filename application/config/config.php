594c21cdbdc5b3bfa4095ce98ed59acee3f29b01
blackraspberry08 <carlo.valencia066@gmail.com>
2019-12-16 20:06:06 +0800

    Lowercased base_url on config

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://localhost/LCC-Eshop/';
+$config['base_url'] = 'http://localhost/lcc-eshop/';
 
 /*
 |--------------------------------------------------------------------------
