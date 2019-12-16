b50f24bbf69c90a706eb468c4ba7fd77ce8d772b
blackraspberry08 <carlo.valencia066@gmail.com>
2019-12-16 21:30:08 +0800

    Limit payment option to Cash on Delivery only.

--- application/views/web/pages/checkout.php
+++ application/views/web/pages/checkout.php
@@ -12,9 +12,11 @@
                 <p><?php echo $this->session->flashdata('message'); ?></p>
             </div>
             <form method="post" action="<?php echo base_url('save/order');?>" style="text-align: left">
-                <span><input type="radio" name="payment" value="cashon"/>Cash On Delivary</span><br/>
+                <span><input type="radio" name="payment" value="cashon" checked/>Cash On Delivery</span><br/>
+                <!--
                 <span><input type="radio" name="payment" value="ssl"/>SSL Wocommerz</span><br/>
                 <span><input type="radio" name="payment" value="paypal"/>Paypal</span><br/><br/>
+                -->
                 <div class="search"><div><button class="grey">Create Account</button></div></div>
             </form>
            
